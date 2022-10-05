12579307709630277a4b7d8e51ea31c27f06f721
RicardoJiang <2868405029@qq.com>
2022-10-05 20:38:49 +0800

    ADD: test1

--- app/src/main/java/com/zj/android/workflow/MainActivity.kt
+++ app/src/main/java/com/zj/android/workflow/MainActivity.kt
@@ -3,10 +3,12 @@ package com.zj.android.workflow
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.zj.workflow.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_test.text = ""
     }
 }
