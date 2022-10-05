1a06a503e3db1e9e548397a864c370a063a602f5
RicardoJiang <2868405029@qq.com>
2022-10-05 23:35:37 +0800

    ADD: test1

--- app/src/main/java/com/zj/android/workflow/MainActivity.kt
+++ app/src/main/java/com/zj/android/workflow/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.zj.android.workflow
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.zj.workflow.R
 
@@ -8,5 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
     }
 }
