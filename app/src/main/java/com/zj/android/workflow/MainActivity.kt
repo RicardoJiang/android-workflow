ab8a0913fe69f44ffb1eb371b81f037558d56cfc
RicardoJiang <2868405029@qq.com>
2022-10-06 08:27:59 +0800

    FIX: detekt problem

--- app/src/main/java/com/zj/android/workflow/MainActivity.kt
+++ app/src/main/java/com/zj/android/workflow/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.zj.android.workflow
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.zj.workflow.R
 
@@ -9,6 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
     }
 }
